1f2c87e785a0843d46de3a2ea95e1fc385334935
olachinkei <kei.kyoto.be.amb@gmail.com>
2023-10-09 23:11:10 +0900

    update

--- src/jp-finetuning/japanese-task-evaluation.py
+++ src/jp-finetuning/japanese-task-evaluation.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import wandb
 import sentencepiece
+import argparse
 from datasets import load_dataset, load_from_disk
 #from wandb.integration.langchain import WandbTracer
 from langchain.callbacks.tracers import WandbTracer
@@ -15,32 +16,22 @@ from huggingface_hub.inference_api import InferenceApi
 from huggingface_hub import login
 from prompt_template import get_template
 from utils import eval_MARC_ja, eval_JSTS, eval_JNLI, eval_JSQuAD, eval_JCommonsenseQA
+from peft import PeftModel, PeftConfig, LoraConfig, get_peft_model
 
 
-"can be changed in W&B Launch's setting"
-config = dict(
-    wandb_project="LLM_evaluation_Japan_public",
-    wandb_entity="wandb",
-    model_name="cyberagent/open-calm-small",
-    prompt_type="other",
-    use_artifact = False
-    )
-
-login(os.environ['HUGGINGFACE_TOKEN'])
+#login(os.environ['HUGGINGFACE_TOKEN'])
 
 def parse_args():
     parser = argparse.ArgumentParser(description="upload raw score json file")
     parser.add_argument(
         "--wandb_entity",
         type=str,
-        #defalt="wandb",
         required="True",
         help="wandb entity name",
     )
     parser.add_argument(
         "--wandb_project",
         type=str,
-        #default="LLM_evaluation_Japan",
         required="True",
         help="wandb project name",
     )
@@ -51,16 +42,10 @@ def parse_args():
         help="yes or no",
     )
     parser.add_argument(
-        "--peft_type",
+        "--adapter_artifact_name",
         type=str,
         required="False",
-        help="type of peft: lora (now adaptor of lora can be used)",
-    )
-    parser.add_argument(
-        "--model_artifact_name",
-        type=str,
-        required="False",
-        help="artifact path of adaptor",
+        help="artifact path of adapter",
     )
     parser.add_argument(
         "--hf_model_name",
@@ -75,7 +60,7 @@ def parse_args():
         help="Used as the displayed name on leaderboard",
     )
     parser.add_argument(
-        "--use_aftifact_of_dataset",
+        "--use_artifact_of_dataset",
         type=str,
         required="True",
         help="yes or no",
@@ -92,8 +77,8 @@ def parse_args():
 
 if __name__ == "__main__":
     eval_category = ['MARC-ja', 'JSTS', 'JNLI', 'JSQuAD', 'JCommonsenseQA']
-    
-    with wandb.init(project=arg.wandb_project, entity=arg.wandb_entity, config=arg, job_type="eval") as run:
+    args = parse_args()
+    with wandb.init(project=args.wandb_project, entity=args.wandb_entity, config=args, job_type="eval") as run:
         config = wandb.config
         table_contents = []
         table_contents.append(config["model_name"])
@@ -106,20 +91,19 @@ if __name__ == "__main__":
             temperature = 1e-9
         else:
             temperature = 0
-        template_type = config.prompt
+        template_type = config.prompt_template_type
 
-        if use_local_model=="no":
+        if config.use_local_model=="no":
             model = AutoModelForCausalLM.from_pretrained(config.hf_model_name, trust_remote_code=True,torch_dtype=torch.float16)
         else:
             base_llm = AutoModelForCausalLM.from_pretrained(config.hf_model_name, trust_remote_code=True, torch_dtype=torch.float16)
-            model_ar = wandb.use_artifact(config.model_artifact_name)
+            model_ar = wandb.use_artifact(config.adapter_artifact_name)
             model_path = model_ar.download()
             model = PeftModel.from_pretrained(base_llm, model_path, torch_dtype=torch.float16)
             model = model.merge_and_unload()
             
-
         #MRAC-ja --------------------------------------------------------
-        if config.use_artifact:
+        if config.use_artifact_of_dataset=="yes":
             artifact = run.use_artifact('wandb/LLM_evaluation_Japan/JGLUE-MRAC-ja:v0', type='dataset')
             artifact_dir = artifact.download()
             dataset = load_from_disk(artifact_dir)
@@ -127,14 +111,16 @@ if __name__ == "__main__":
             dataset = load_dataset("shunk031/JGLUE", name=eval_category[0])
         pipe = pipeline(
             "text-generation", model=model, tokenizer=tokenizer, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.pad_token_id,
-            max_new_tokens=5, device=0, torch_dtype=torch.float16, temperature=temperature, 
+            max_new_tokens=5, device=0, torch_dtype=torch.bfloat16, temperature=temperature, 
         )
         llm = HuggingFacePipeline(pipeline=pipe)
         llm_chain = LLMChain(llm=llm, prompt=get_template(eval_category[0], template_type), output_key="output")
-        marc_ja_score = eval_MARC_ja(dataset,llm_chain)
+        marc_ja_score,marc_ja_score_balanced  = eval_MARC_ja(dataset,llm_chain)
         table_contents.append(marc_ja_score)
+        table_contents.append(marc_ja_score_balanced)
+        
         #JSTS--------------------------------------------------------
-        if config.use_artifact:
+        if config.use_artifact_of_dataset=="yes":
             artifact = run.use_artifact('wandb/LLM_evaluation_Japan/JGLUE-JSTS:v0', type='dataset')
             artifact_dir = artifact.download()
             dataset = load_from_disk(artifact_dir)
@@ -142,7 +128,7 @@ if __name__ == "__main__":
             dataset = load_dataset("shunk031/JGLUE", name=eval_category[1])
         pipe = pipeline(
             "text-generation", model=model, tokenizer=tokenizer, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.pad_token_id,
-            max_new_tokens=3, device=0, torch_dtype=torch.float16, temperature=temperature,
+            max_new_tokens=3, device=0, torch_dtype=torch.bfloat16, temperature=temperature,
         )
         llm = HuggingFacePipeline(pipeline=pipe)
         llm_chain = LLMChain(llm=llm, prompt=get_template(eval_category[1], template_type), output_key="output")
@@ -150,7 +136,7 @@ if __name__ == "__main__":
         table_contents.append(jsts_peason)
         table_contents.append(jsts_spearman)
         #JNLI--------------------------------------------------------
-        if config.use_artifact:
+        if config.use_artifact_of_dataset=="yes":
             artifact = run.use_artifact('wandb/LLM_evaluation_Japan/JGLUE-JNLI:v0', type='dataset')
             artifact_dir = artifact.download()
             dataset = load_from_disk(artifact_dir)
@@ -158,15 +144,16 @@ if __name__ == "__main__":
             dataset = load_dataset("shunk031/JGLUE", name=eval_category[2])
         pipe = pipeline(
             "text-generation", model=model, tokenizer=tokenizer, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.pad_token_id,
-            max_new_tokens=3, device=0, torch_dtype=torch.float16, temperature=temperature,
+            max_new_tokens=3, device=0, torch_dtype=torch.bfloat16, temperature=temperature,
         )
         llm = HuggingFacePipeline(pipeline=pipe)
         llm_chain = LLMChain(llm=llm, prompt=get_template(eval_category[2], template_type), output_key="output")
-        jnli_score = eval_JNLI(dataset,llm_chain)
+        jnli_score,jnli_score_balanced = eval_JNLI(dataset,llm_chain)
         table_contents.append(jnli_score)
+        table_contents.append(jnli_score_balanced)
 
         #JSQuAD--------------------------------------------------------
-        if config.use_artifact:
+        if config.use_artifact_of_dataset=="yes":
             artifact = run.use_artifact('wandb/LLM_evaluation_Japan/JGLUE-JSQuAD:v0', type='dataset')
             artifact_dir = artifact.download()
             dataset = load_from_disk(artifact_dir)
@@ -174,7 +161,7 @@ if __name__ == "__main__":
             dataset = load_dataset("shunk031/JGLUE", name=eval_category[3])
         pipe = pipeline(
             "text-generation", model=model, tokenizer=tokenizer, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.pad_token_id,
-            max_new_tokens=25, device=0, torch_dtype=torch.float16, temperature=temperature,
+            max_new_tokens=25, device=0, torch_dtype=torch.bfloat16, temperature=temperature,
         )
         llm = HuggingFacePipeline(pipeline=pipe)
         llm_chain = LLMChain(llm=llm, prompt=get_template(eval_category[3], template_type), output_key="output")
@@ -184,7 +171,7 @@ if __name__ == "__main__":
         table_contents.append(JSQuAD_F1)
  
         #JCommonsenseQA--------------------------------------------------------
-        if config.use_artifact:
+        if config.use_artifact_of_dataset=="yes":
             artifact = run.use_artifact('wandb/LLM_evaluation_Japan/JGLUE-JCommonsenseQA:v0', type='dataset')
             artifact_dir = artifact.download()
             dataset = load_from_disk(artifact_dir)
@@ -192,7 +179,7 @@ if __name__ == "__main__":
             dataset = load_dataset("shunk031/JGLUE", name=eval_category[4])
         pipe = pipeline(
             "text-generation", model=model, tokenizer=tokenizer, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.pad_token_id,
-            max_new_tokens=5, device=0, torch_dtype=torch.float16, temperature=temperature,
+            max_new_tokens=5, device=0, torch_dtype=torch.bfloat16, temperature=temperature,
             )
         llm = HuggingFacePipeline(pipeline=pipe)
         llm_chain = LLMChain(llm=llm, prompt=get_template(eval_category[4], template_type), output_key="output")
@@ -201,10 +188,12 @@ if __name__ == "__main__":
         table_contents.append(JCommonsenseQA)
 
         #End--------------------------------------------------------
-        table = wandb.Table(columns=['model_name ','MARC-ja', 'JSTS-pearson', 'JSTS-spearman', 'JNLI', 'JSQuAD-EM', 'JSQuAD-F1', 'JCommonsenseQA'] ,
+        table = wandb.Table(columns=['model_name ','MARC-ja','MARC-ja-balanced', 'JSTS-pearson',
+                                     'JSTS-spearman', 'JNLI','JNLI-balanced', 'JSQuAD-EM', 'JSQuAD-F1', 'JCommonsenseQA'],
                             data=[table_contents])
-        table = wandb.Table(columns=['model_name ','MARC-ja', 'JSTS-pearson', 'JSTS-spearman', 'JNLI', 'JSQuAD-EM', 'JSQuAD-F1', 'JCommonsenseQA'] ,
+        table = wandb.Table(columns=['model_name ','MARC-ja','MARC-ja-balanced', 'JSTS-pearson',
+                                     'JSTS-spearman', 'JNLI','JNLI-balanced', 'JSQuAD-EM', 'JSQuAD-F1', 'JCommonsenseQA'],
                             data=table.data)
-        run.log({'result_table':table}) 
+        run.log({'result_table_balanced':table}) 
         run.log_code()
 
